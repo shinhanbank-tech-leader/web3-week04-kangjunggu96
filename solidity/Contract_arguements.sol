b5f1e9e977254bfa6dc4ec21a11e00db3ffb74ab
kangjunggu96 <justinkang110432@gmail.com>
2026-03-29 22:48:02 +0900

    commit

---
+++ solidity/Contract_arguements.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    uint public x;
+    constructor(uint _x){
+        x = _x;
+    }
+    function increment() public{
+        x+=1;
+    }
+
+    function add(uint parameter) external returns(uint){
+        return parameter + x;
+    }
+}
